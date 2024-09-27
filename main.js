55f04e0500eed7ac21827f92a1627b8675a6b197
Daniel <daniel.rezende@alunoescolassociais.org.br>
2024-09-27 09:47:50 -0300

    adicionei a função consumo de APIViaCEP

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-'use strict'; // Ativa o modo restrito
+use strict'; // Ativa o modo restrito
 // Código para cosumo de API da ViaCEP
 // https://viacep.com.br/
  
@@ -11,15 +11,39 @@ const limparFormulario = () =>{
     document.getElementById('estado').value ='';
 }
  
+ 
 // Verificar se o CEP é válido
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 // Verifica o tamanho CEP
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
-
-//Função para preencher campos relacionados ao cep
-const preencherFotmulario = (endereco) =>{
-    document.getElementById("logradouro").value = endereco.logradouro;
-    document.getElementById("bairro").value = endereco.bairro;
-    document.getElementById("localidade").value = endereco.localidade;
-    document.getElementById("uf").value = endereco.uf;
+ 
+// Função para preencher campos relacionados ao CEP
+const preencherFormulario = (endereco) =>{
+    document.getElementById('logradouro').value = endereco.logradouro;
+    //Colocar o valor de logradouro da API dentro do campo logradouro do formulário
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('localidade').value = endereco.localidade;
+    document.getElementById('uf').value = endereco.uf;
 }
+ 
+ 
+//Função para consumo de APIViaCEP
+const pesquisarCep = async() => {
+    limparFormulario();
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+    if(cepValido(cep.value)){
+        const dados = await fetch(url);
+        const addres = await dados.json();
+ 
+        if(addres.hasOwnProperty('erro')){
+            alert('CEP não encontrado');
+        }else{
+            preencherFormulario(addres);
+        }
+    }else{
+        alert('CEP incorreto');
+    }
+}
+ 
+// Executa a ação de preenchimento do formulario ao deixar o campo do CEP
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
